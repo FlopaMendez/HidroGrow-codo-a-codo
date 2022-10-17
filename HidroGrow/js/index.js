4033ac0ad845d5dfc008134d6d97b8c7de5cc9f9
Nicolás Villamor <villamorn@hotmail.com>
2022-10-17 17:34:10 -0300

    17-10-2022

--- HidroGrow/js/index.js
+++ HidroGrow/js/index.js
@@ -25,4 +25,9 @@ punto.forEach( ( cadaPunto , i )=> {
     })
 })
 
-
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
