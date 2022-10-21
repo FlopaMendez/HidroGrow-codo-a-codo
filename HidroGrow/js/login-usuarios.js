ad697726cbd79f53ba4a79530a27b14ec8e0f409
Pablo Alustiza <alustiza.p@gmail.com>
2022-10-21 19:36:55 -0300

    ok

--- HidroGrow/js/login-usuarios.js
+++ HidroGrow/js/login-usuarios.js
@@ -39,6 +39,18 @@ function traer() {
 
 
 
+const div = document.querySelector('div')
+
+document.querySelector('.hide').addEventListener('click',() => {
+  div.classList.add('div_hide')
+})
+
+document.querySelector('.show').addEventListener('click',() => {
+  div.classList.remove('div_hide')
+})
+
+
+
 
 
 
