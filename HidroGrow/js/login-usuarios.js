4f727dd94e9e8937bfe9a2c58ba81a36a805be78
Pablo Alustiza <alustiza.p@gmail.com>
2022-10-24 20:45:38 -0300

    update style-usuarios.css

--- HidroGrow/js/login-usuarios.js
+++ HidroGrow/js/login-usuarios.js
@@ -3,27 +3,27 @@ var contenidoDevice = document.querySelector('#contenidoDevice');
 
 function traer() {
 
-    fetch('https://randomuser.me/api/') 
+  fetch('https://randomuser.me/api/')
 
-        .then(res => res.json())
-        .then(data => {
-            contenidoUsr.innerHTML =
-                `
-                        <p></p>
-                        <img class="img-circle" src="${data.results[0].picture.large}" width="100px" class="img-fluid rounded-circle">
-                        <p >Nombre: ${data.results[0].name.first}</p>
-                        <p >Email: ${data.results[0].email}</p>
+    .then(res => res.json())
+    .then(data => {
+      contenidoUsr.innerHTML =
+        `
+                        <div class="divUsr">
+                        <img class="imgUsr" src="${data.results[0].picture.large}"  >
+                        <p>Nombre: ${data.results[0].name.first}<br>
+                        Email: ${data.results[0].email}</p>
                         <br>
-
+                        </div>
                         `
-        }),
-        fetch('https://raw.githubusercontent.com/FlopaMendez/HidroGrow-codo-a-codo/usuarios-dispositivos/HidroGrow/json/dispositivos.json') 
+    }),
+    fetch('https://raw.githubusercontent.com/FlopaMendez/HidroGrow-codo-a-codo/usuarios-dispositivos/HidroGrow/json/dispositivos.json')
 
-            .then(res2 => res2.json())
-            .then(data2 => {
+      .then(res2 => res2.json())
+      .then(data2 => {
 
-                contenidoDevice.innerHTML =
-                    `
+        contenidoDevice.innerHTML =
+          `
                     <p>UNIDADES DE CULTIVO</p>
                 <h1 >${data2.dispositivos[0].name} </h1>
                 <h2>Temperatura: ${data2.dispositivos[0].temperatura[0]}°C</h2>
@@ -31,7 +31,7 @@ function traer() {
                 <p>Número de serie: ${data2.dispositivos[0].serialNumber} </p>
                 <p>Fecha inicio del cultivo: ${data2.dispositivos[0].fechaInicio} </p>
                         `
-            })
+      })
 
 
 
@@ -41,11 +41,11 @@ function traer() {
 
 const div = document.querySelector('div')
 
-document.querySelector('.hide').addEventListener('click',() => {
+document.querySelector('.hide').addEventListener('click', () => {
   div.classList.add('div_hide')
 })
 
-document.querySelector('.show').addEventListener('click',() => {
+document.querySelector('.show').addEventListener('click', () => {
   div.classList.remove('div_hide')
 })
 
