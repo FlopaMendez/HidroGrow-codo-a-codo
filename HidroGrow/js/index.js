38e87506d6fb951c0d1a63e6d29a8a7fd198f830
Nicolás Villamor <villamorn@hotmail.com>
2022-10-24 23:15:14 -0300

    24-10-2022

--- HidroGrow/js/index.js
+++ HidroGrow/js/index.js
@@ -69,14 +69,58 @@ function myFunction(imgs) {
     }
 }
 
+/* HIDE HEADER*/
 
-function HiddenMenu(){
- document.getElementById("encabezado").classList.toggle('no-show');
-}
-window.onload = function() 
-{
-  document.getElementById("encabezado").addEventListener( 'click' , HiddenMenu ); 
-}
+(function(){
 
+  var doc = document.documentElement;
+  var w = window;
 
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curScroll;
+  var direction = 0;
+  var prevDirection = 0;
 
+  var header = document.getElementById('site-header');
+
+  var checkScroll = function() {
+
+    /*
+    ** Find the direction of scroll
+    ** 0 - initial, 1 - up, 2 - down
+    */
+
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) { 
+      //scrolled up
+      direction = 2;
+    }
+    else if (curScroll < prevScroll) { 
+      //scrolled down
+      direction = 1;
+    }
+
+    if (direction !== prevDirection) {
+      toggleHeader(direction, curScroll);
+    }
+    
+    prevScroll = curScroll;
+  };
+
+  var toggleHeader = function(direction, curScroll) {
+    if (direction === 2 && curScroll > 80) { 
+      
+      //replace 52 with the height of your header in px
+
+      encabezado.classList.add('hide');
+      prevDirection = direction;
+    }
+    else if (direction === 1) {
+      encabezado.classList.remove('hide');
+      prevDirection = direction;
+    }
+  };
+  
+  window.addEventListener('scroll', checkScroll);
+
+})();
